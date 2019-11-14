70d128371aba092416c840db5021a8e2b26a3207
moniecodes <mona24@gmail.com>
2019-11-14 18:13:23 -0500

    - pxtarget.json updates to pull sim and variant definitions from https://github.com/microsoft/pxt-maker/blob/master/pxtarget.json - Simulator code pulled from https://github.com/microsoft/pxt-maker/tree/master/sim

--- sim/visuals/boardview.ts
+++ sim/visuals/boardview.ts
@@ -1,10 +1,12 @@
 namespace pxsim.visuals {
-    mkBoardView = (opts: BoardViewOptions): BoardView => {
-        return new visuals.CircuitPlaygroundBoardSvg({
+    mkBoardView = (opts: pxsim.visuals.BoardViewOptions): BoardView => {
+        return new visuals.MetroBoardSvg({
             runtime: runtime,
             theme: visuals.randomTheme(),
+            visualDef: opts.visual as BoardImageDefinition,
+            boardDef: opts.boardDef,
             disableTilt: false,
-            wireframe: opts.wireframe,
+            wireframe: opts.wireframe
         });
     }
 }
