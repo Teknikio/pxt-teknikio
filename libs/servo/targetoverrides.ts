6f96f425b31104034028e17381272beae02620c7
Joe Bakalor <jmbakalor@gmail.com>
2020-11-25 11:37:39 -0500

    Udpate/temp (#97)
    * add servo
    
    * serfvo
    
    * dd
    
    * add servo
    
    * temp update

--- libs/servo/targetoverrides.ts
+++ libs/servo/targetoverrides.ts
@@ -1,6 +1,6 @@
 namespace servos {
-    //% block="servo A1" fixedInstance
+    //% block="servo SERVO_LEFT" fixedInstance
     export const A1 = new servos.PinServo(pins.SERVO_LEFT);
-    //% block="servo A2" fixedInstance
+    //% block="servo SERVO_RIGHT" fixedInstance
     export const A2 = new servos.PinServo(pins.SERVO_RIGHT);
 }
