705dd99a09f6a2ab5f882d50755e685fa65ce80b
JoeBakalor <jmbakalor@gmail.com>
2021-05-25 13:25:57 -0400

    update servo pins

--- libs/servo/targetoverrides.ts
+++ libs/servo/targetoverrides.ts
@@ -1,6 +1,6 @@
 namespace servos {
     //% block="servo A1" fixedInstance
-    export const A1 = new servos.PinServo(pins.A1);
+    export const A1 = new servos.PinServo(pins.A3);
     //% block="servo A2" fixedInstance
-    export const A2 = new servos.PinServo(pins.A2);
+    export const A2 = new servos.PinServo(pins.A4);
 }
